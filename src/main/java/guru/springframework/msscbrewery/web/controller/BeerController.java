dba3abab5025c05e4c61270fd2480a7f1b4cad4a
aaron <aaron.antony139@gmail.com>
2020-12-10 15:51:07 +0530

    Date Mapper

--- src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
@@ -2,6 +2,8 @@ package guru.springframework.msscbrewery.web.controller;
 
 import guru.springframework.msscbrewery.services.BeerService;
 import guru.springframework.msscbrewery.web.model.BeerDto;
+import lombok.RequiredArgsConstructor;
+import lombok.val;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,47 +15,42 @@ import java.util.UUID;
 /**
  * Created by jt on 2019-04-20.
  */
-@Deprecated
-@RequestMapping("/api/v1/beer")
+@RequestMapping("api/v1/beer")
 @RestController
+@RequiredArgsConstructor
 public class BeerController {
 
     private final BeerService beerService;
 
-    public BeerController(BeerService beerService) {
-        this.beerService = beerService;
-    }
-
     @GetMapping({"/{beerId}"})
-    public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId){
-
+    public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId) {
+        val mine = beerService.getBeerById(beerId);
+        System.out.println(mine);
         return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
     }
 
-    @PostMapping // POST - create new beer
-    public ResponseEntity handlePost(@Valid @RequestBody BeerDto beerDto){
-
+    @PostMapping
+    public ResponseEntity handlePost(@Valid @RequestBody BeerDto beerDto) {
+        System.out.println("In here");
         BeerDto savedDto = beerService.saveNewBeer(beerDto);
-
         HttpHeaders headers = new HttpHeaders();
-        //todo add hostname to url
-        headers.add("Location", "/api/v1/beer/" + savedDto.getId().toString());
-
-        return new ResponseEntity(headers, HttpStatus.CREATED);
+        headers.add("Location", "api/v1/beer/" + savedDto.getId().toString());
+        ResponseEntity s = new ResponseEntity(headers, HttpStatus.CREATED);
+        System.out.println("This is is s: " + s);
+        return s;
     }
 
     @PutMapping({"/{beerId}"})
-    public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId, @Valid @RequestBody BeerDto beerDto){
-
+    public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId, @Valid @RequestBody BeerDto beerDto) {
         beerService.updateBeer(beerId, beerDto);
-
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
     @DeleteMapping({"/{beerId}"})
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteBeer(@PathVariable("beerId") UUID beerId){
+//    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public void deleteBeer(@PathVariable("beerId") UUID beerId) {
+
         beerService.deleteById(beerId);
     }
-
 }
